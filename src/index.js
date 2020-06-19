4075e9e65d65f2eaad2165a7d43e4d6a08ad7ce4
Shahinur-Rahman71 <shanto16026@gmail.com>
2020-06-19 11:10:07 +0600

    ReactJs With Bootstrap

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import './assets/css/bootstrap.min.css'
+import './assets/css/agency.min.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
